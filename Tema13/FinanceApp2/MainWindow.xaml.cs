1e1eb90c84abed1d8fa54e5ff64addb491a19199
sedenevskijmiroslav-boop <sedenevskijmiroslav@gmail.com>
2026-03-31 22:54:37 +0300

    Added Tema13

---
+++ Tema13/FinanceApp2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using FinanceApp2.ViewModels;
+
+namespace FinanceApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Report_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as MainViewModel;
+            if (vm != null && vm.Transactions.Count > 0)
+            {
+                decimal total = vm.Transactions.Sum(t => t.Type == "Доход" ? t.Amount : -t.Amount);
+                decimal income = vm.Transactions.Where(t => t.Type == "Доход").Sum(t => t.Amount);
+                decimal expense = vm.Transactions.Where(t => t.Type == "Расход").Sum(t => t.Amount);
+
+                MessageBox.Show($"Общий отчет\n\nДоходы: {income:C}\nРасходы: {expense:C}\nИтого: {total:C}",
+                    "Отчет", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Нет данных для отчета", "Отчет", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void ReportByType_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as MainViewModel;
+            if (vm != null)
+            {
+                var income = vm.Transactions.Where(t => t.Type == "Доход").Sum(t => t.Amount);
+                var expense = vm.Transactions.Where(t => t.Type == "Расход").Sum(t => t.Amount);
+
+                MessageBox.Show($"Отчет по типам\n\nДоходы: {income:C}\nРасходы: {expense:C}",
+                    "Отчет", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Учет финансов\nВерсия 1.0\n\nПрограмма для учета доходов и расходов\n\nГорячие клавиши:\nCtrl+N - Добавить\nCtrl+E - Редактировать\nDel - Удалить",
+                "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Tema13/FinanceApp2/ViewModels/MainViewModels.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using FinanceApp2.Models;
+
+namespace FinanceApp2.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Transaction> Transactions { get; set; }
+        public Transaction SelectedTransaction { get; set; }
+
+        public ICommand AddTransactionCommand { get; set; }
+        public ICommand EditTransactionCommand { get; set; }
+        public ICommand DeleteTransactionCommand { get; set; }
+
+        private int _nextId = 1;
+
+        public MainViewModel()
+        {
+            Transactions = new ObservableCollection<Transaction>();
+
+            AddTransactionCommand = new RelayCommand(ExecuteAdd);
+            EditTransactionCommand = new RelayCommand(ExecuteEdit, CanExecuteEditDelete);
+            DeleteTransactionCommand = new RelayCommand(ExecuteDelete, CanExecuteEditDelete);
+
+            Transactions.Add(new Transaction
+            {
+                Id = _nextId++,
+                Amount = 50000,
+                Type = "Доход",
+                Date = DateTime.Now,
+                Description = "Зарплата"
+            });
+
+            Transactions.Add(new Transaction
+            {
+                Id = _nextId++,
+                Amount = 3000,
+                Type = "Расход",
+                Date = DateTime.Now,
+                Description = "Продукты"
+            });
+        }
+
+        private bool CanExecuteEditDelete(object parameter)
+        {
+            return SelectedTransaction != null;
+        }
+
+        private void ExecuteAdd(object parameter)
+        {
+            var dialog = new TransactionDialog();
+            dialog.Owner = Application.Current.MainWindow;
+
+            if (dialog.ShowDialog() == true)
+            {
+                var newTransaction = new Transaction
+                {
+                    Id = _nextId++,
+                    Amount = dialog.Amount,
+                    Type = dialog.Type,
+                    Date = dialog.Date,
+                    Description = dialog.Description
+                };
+
+                Transactions.Add(newTransaction);
+            }
+        }
+
+        private void ExecuteEdit(object parameter)
+        {
+            if (SelectedTransaction == null) return;
+
+            var dialog = new TransactionDialog(SelectedTransaction);
+            dialog.Owner = Application.Current.MainWindow;
+
+            if (dialog.ShowDialog() == true)
+            {
+                SelectedTransaction.Amount = dialog.Amount;
+                SelectedTransaction.Type = dialog.Type;
+                SelectedTransaction.Date = dialog.Date;
+                SelectedTransaction.Description = dialog.Description;
+
+                int index = Transactions.IndexOf(SelectedTransaction);
+                Transactions[index] = SelectedTransaction;
+            }
+        }
+
+        private void ExecuteDelete(object parameter)
+        {
+            if (SelectedTransaction == null) return;
+
+            var result = MessageBox.Show(
+                $"Удалить транзакцию?\n\nДата: {SelectedTransaction.Date:dd.MM.yyyy}\nТип: {SelectedTransaction.Type}\nСумма: {SelectedTransaction.Amount:C}",
+                "Подтверждение удаления",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Transactions.Remove(SelectedTransaction);
+            }
+        }
+    }
+}
