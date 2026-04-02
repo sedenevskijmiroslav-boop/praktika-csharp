376b36423793c7d2b131bb69ba8a3acedf6ebab1
sedenevskijmiroslav-boop <sedenevskijmiroslav@gmail.com>
2026-04-02 22:27:21 +0300

    Added Tema14

---
+++ Tema14/FinanceApp2/TransactionDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using FinanceApp2.Models;
+
+namespace FinanceApp2
+{
+    public partial class TransactionDialog : Window
+    {
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public TransactionDialog(Transaction transaction = null)
+        {
+            InitializeComponent();
+
+            if (transaction != null)
+            {
+                txtAmount.Text = transaction.Amount.ToString();
+
+                if (transaction.Type == "Доход")
+                    cmbType.SelectedIndex = 0;
+                else
+                    cmbType.SelectedIndex = 1;
+
+                dpDate.SelectedDate = transaction.Date;
+                txtDescription.Text = transaction.Description;
+            }
+            else
+            {
+                dpDate.SelectedDate = DateTime.Now;
+                cmbType.SelectedIndex = 0;
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtAmount.Text, out decimal amount) && amount > 0)
+            {
+                Amount = amount;
+                Type = (cmbType.SelectedItem as ComboBoxItem)?.Content.ToString();
+                Date = dpDate.SelectedDate ?? DateTime.Now;
+                Description = txtDescription.Text;
+
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректную сумму", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ Tema14/FinanceApp2/ViewModels/MainViewModels.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using FinanceApp2.Models;
+
+namespace FinanceApp2.ViewModels;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private ObservableCollection<Transaction> _allTransactions;
+    private ObservableCollection<Transaction> _filteredTransactions;
+    private DateTime? _filterDate;
+    private Transaction _selectedTransaction;
+    private decimal _balance;
+
+    public ObservableCollection<Transaction> Transactions
+    {
+        get => _filteredTransactions;
+        set
+        {
+            _filteredTransactions = value;
+            OnPropertyChanged(nameof(Transactions));
+        }
+    }
+
+    public Transaction SelectedTransaction
+    {
+        get => _selectedTransaction;
+        set
+        {
+            _selectedTransaction = value;
+            OnPropertyChanged(nameof(SelectedTransaction));
+            ((RelayCommand)EditTransactionCommand).RaiseCanExecuteChanged();
+            ((RelayCommand)DeleteTransactionCommand).RaiseCanExecuteChanged();
+        }
+    }
+
+    public decimal Balance
+    {
+        get => _balance;
+        set
+        {
+            _balance = value;
+            OnPropertyChanged(nameof(Balance));
+        }
+    }
+
+    public DateTime? FilterDate
+    {
+        get => _filterDate;
+        set
+        {
+            _filterDate = value;
+            OnPropertyChanged(nameof(FilterDate));
+            ApplyFilter();
+        }
+    }
+
+    public ICommand AddTransactionCommand { get; }
+    public ICommand EditTransactionCommand { get; }
+    public ICommand DeleteTransactionCommand { get; }
+    public ICommand ClearFilterCommand { get; }
+
+    private int _nextId = 3;
+
+    public MainViewModel()
+    {
+        _allTransactions = new ObservableCollection<Transaction>();
+        _filteredTransactions = new ObservableCollection<Transaction>();
+
+        _allTransactions.Add(new Transaction { Id = 1, Amount = 50000, Type = "Доход", Date = DateTime.Now, Description = "Зарплата" });
+        _allTransactions.Add(new Transaction { Id = 2, Amount = 3000, Type = "Расход", Date = DateTime.Now, Description = "Продукты" });
+
+        ApplyFilter();
+        UpdateBalance();
+
+        AddTransactionCommand = new RelayCommand(ExecuteAdd);
+        EditTransactionCommand = new RelayCommand(ExecuteEdit, CanExecuteEditDelete);
+        DeleteTransactionCommand = new RelayCommand(ExecuteDelete, CanExecuteEditDelete);
+        ClearFilterCommand = new RelayCommand(ExecuteClearFilter);
+    }
+
+    private bool CanExecuteEditDelete(object parameter)
+    {
+        return SelectedTransaction != null;
+    }
+
+    private void ExecuteAdd(object parameter)
+    {
+        var dialog = new TransactionDialog();
+        dialog.Owner = Application.Current.MainWindow;
+
+        if (dialog.ShowDialog() == true)
+        {
+            var transaction = new Transaction
+            {
+                Id = _nextId++,
+                Amount = dialog.Amount,
+                Type = dialog.Type,
+                Date = dialog.Date,
+                Description = dialog.Description
+            };
+
+            _allTransactions.Add(transaction);
+            ApplyFilter();
+            UpdateBalance();
+        }
+    }
+
+    private void ExecuteEdit(object parameter)
+    {
+        if (SelectedTransaction == null) return;
+
+        var dialog = new TransactionDialog(SelectedTransaction);
+        dialog.Owner = Application.Current.MainWindow;
+
+        if (dialog.ShowDialog() == true)
+        {
+            SelectedTransaction.Amount = dialog.Amount;
+            SelectedTransaction.Type = dialog.Type;
+            SelectedTransaction.Date = dialog.Date;
+            SelectedTransaction.Description = dialog.Description;
+
+            int index = _allTransactions.IndexOf(SelectedTransaction);
+            if (index >= 0)
+            {
+                _allTransactions[index] = SelectedTransaction;
+            }
+
+            ApplyFilter();
+            UpdateBalance();
+        }
+    }
+
+    private void ExecuteDelete(object parameter)
+    {
+        if (SelectedTransaction == null) return;
+
+        var result = MessageBox.Show(
+            $"Удалить транзакцию?\n\n{SelectedTransaction.Date:dd.MM.yyyy} - {SelectedTransaction.Type} - {SelectedTransaction.Amount:C}",
+            "Подтверждение",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question);
+
+        if (result == MessageBoxResult.Yes)
+        {
+            _allTransactions.Remove(SelectedTransaction);
+            ApplyFilter();
+            UpdateBalance();
+        }
+    }
+
+    private void ExecuteClearFilter(object parameter)
+    {
+        FilterDate = null;
+    }
+
+    private void ApplyFilter()
+    {
+        Transactions.Clear();
+
+        var filtered = _allTransactions.Where(t => !FilterDate.HasValue || t.Date.Date == FilterDate.Value.Date);
+
+        foreach (var transaction in filtered)
+        {
+            Transactions.Add(transaction);
+        }
+    }
+
+    private void UpdateBalance()
+    {
+        decimal income = _allTransactions.Where(t => t.Type == "Доход").Sum(t => t.Amount);
+        decimal expense = _allTransactions.Where(t => t.Type == "Расход").Sum(t => t.Amount);
+        Balance = income - expense;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
